a3b31bcb1398f96c2124478e19a1a9ccb62cf15d
Akbarshoh <88915989+Akbarshox9129@users.noreply.github.com>
2025-06-14 17:56:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -204,5 +204,5 @@ def admin_submissions():
                 submissions[file] = f.readlines()
     return jsonify(submissions)
 
-if __name__ == "__main__":
+if name == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
